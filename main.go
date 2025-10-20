4bd6ef9b7a3a35860d79d73631822a426a7d78e4
logorithm360 <logorithm360@gmail.com>
2025-10-20 10:24:53 +0300

    output formatting

--- main.go
+++ main.go
@@ -5,11 +5,25 @@ import "fmt"
 func main() {
 	// ticket booking app in Golang
 
+	/*
+		PRINT FORMATTING CONSTRUCTS
+		%v	the value in a default format
+		when printing structs, the plus flag (%+v) adds field names
+
+		%#v	a Go-syntax representation of the value
+			(floating-point infinities and NaNs print as ±Inf and NaN)
+
+		%T	a Go-syntax representation of the type of the value
+
+		%%	a literal percent sign; consumes no value
+
+	*/
+
 	var conference = "Conference"
 	const tickets int = 50
 	var availableTickets = 50
-	fmt.Println("Welcome to cromeon", conference, "ticket booking ")
-	fmt.Println("We have total of", tickets, "and tickets remained are", availableTickets)
+	fmt.Printf("Welcome to cromeon %v ticket booking \n", conference)
+	fmt.Printf("We have total of %v and tickets remained are %v \n", tickets, availableTickets)
 	fmt.Println("Get your tickets here to attend")
 
 }
