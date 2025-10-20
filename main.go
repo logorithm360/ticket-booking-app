59196fcab4006ed9dd42d7b1bf9b237c92b1311c
logorithm360 <logorithm360@gmail.com>
2025-10-20 09:57:43 +0300

    variables

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import "fmt"
+
 func main() {
 	// ticket booking app in Golang
+
+	var conference = "Conference"
+	const tickets int = 50
+	var availableTickets = 50
+	fmt.Println("Welcome to cromeon", conference, "ticket booking ")
+	fmt.Println("We have total of", tickets, "and tickets remained are", availableTickets)
+	fmt.Println("Get your tickets here to attend")
+
 }
