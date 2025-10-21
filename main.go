66db45fee46fc54e4cdb1fadba7b7d8f367263af
logorithm360 <logorithm360@gmail.com>
2025-10-21 12:35:12 +0300

    arrays and slices

--- main.go
+++ main.go
@@ -23,16 +23,16 @@ func main() {
 	*/
 
 	var conference = "Conference"
-	const tickets uint = 50
 	var availableTickets uint = 50
 	fmt.Printf("Welcome to cromeon %v ticket booking \n", conference)
-	fmt.Printf("We have total of %v and tickets remained are %v \n", tickets, availableTickets)
+	fmt.Printf("We have total of %v and tickets remained are %v \n", availableTickets, availableTickets)
 	fmt.Println("Get your tickets here to attend")
 
 	var firstName string
 	var lastName string
 	var email string
 	var userTickets uint
+	var bookings []string
 
 	fmt.Println("Enter your first name: ")
 	// this scan function takes the inputed value and assigns it to a variable pointed to which is firstName
@@ -47,9 +47,14 @@ func main() {
 	fmt.Println("How many tickets do you want?")
 	fmt.Scan(&userTickets)
 
-	availableTickets = tickets - userTickets
+	availableTickets = availableTickets - userTickets
+	bookings = append(bookings, firstName+" "+lastName)
 
 	fmt.Printf("Thank you %v %v for booking %v tickets.\n We will confirm and inform you at %v \n\n", firstName, lastName, userTickets, email)
-	fmt.Printf("%v AVAILABLE FOR THE %v", availableTickets, strings.ToUpper(conference))
+	fmt.Printf("%v AVAILABLE FOR THE %v \n", availableTickets, strings.ToUpper(conference))
+
+	// retrieving values from a slice is the same as in arrays
+	// fmt.Printf("%v booked the ticket \n\n", bookings[0])
+	fmt.Printf("NAMES THAT BOOKED TICKETS ARE:\n %v \n", strings.Join(bookings, " "))
 
 }
